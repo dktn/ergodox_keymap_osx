4776c07131f0dd41537305df7e5c25f573d94c39
Adam Szlachta <adam.szlachta@gmail.com>
2017-09-02 18:32:46 +0200

    Experiment 1 in use

--- keymap.c
+++ keymap.c
@@ -13,16 +13,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  ESC   |   1  |   2  |   3  |   4  |   5  | ENT  |           |      |   6  |   7  |   8  |   9  |   0  |  ESC   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |    `   |   Q  |   W  |   D  |   P  |   G  | DELT |           | BTN1 |   J  |   L  |   U  |   Y  |   ;  |   -    |
+ * |    `   |   Q  |   W  |   D  |   P  |   G  | DELT |           | L2   |   J  |   L  |   U  |   Y  |   ;  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  TAB   |   A  |   R  |   S  |   T  |   F  |------|           |------|   M  |   N  |   E  |   I  |   O  |  ENT   |
- * |--------+------+------+------+------+------| L1   |           | L1   |------+------+------+------+------+--------|
- * |   '    |   Z  |   X  |   C  |   V  |   B  |      |           |      |   K  |   H  |   ,  |   .  |  /   |   \    |
+ * |  L1    |   A  |   R  |   S  |   T  |   F  |------|           |------|   M  |   N  |   E  |   I  |   O  |   L1   |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  TAB   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   K  |   H  |   ,  |   .  | ENT  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | WH_D | WH_U | LEFT | RIGHT| BSPC |                                       | SPC  | UP   | DOWN |  =   |      |
+ *   | WH_D | WH_U | LEFT | RIGHT| BSPC |                                       | SPC  | UP   | DOWN |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | BTN2 | BTN1 |       | L2   | BTN2 |
+ *                                        | BTN2 | BTN1 |       | BTN1 | BTN2 |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      | LCTL |       | RCTL |      |      |
  *                                 |LSHFT | LGUI |------|       |------| RGUI |RSHFT |
@@ -31,36 +31,66 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
+
+// OLS vs. MO
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,    KC_3,    KC_4,   KC_5,    KC_ENT,
-        KC_GRV,         KC_Q,         KC_W,    KC_D,    KC_P,   KC_G,    KC_DELT,
-        KC_TAB,         KC_A,         KC_R,    KC_S,    KC_T,   KC_F,
-        KC_QUOT,        KC_Z,         KC_X,    KC_C,    KC_V,   KC_B,    MO(SYMB),
-        KC_WH_D,        KC_WH_U,      KC_LEFT, KC_RGHT, KC_BSPC,
+        KC_ESC,         KC_1,      KC_2,    KC_3,    KC_4,   KC_5,    KC_ENT,
+        KC_GRV,         KC_Q,      KC_W,    KC_D,    KC_P,   KC_G,    KC_DELT,
+        MO(SYMB),       KC_A,      KC_R,    KC_S,    KC_T,   KC_F,
+        KC_TAB,         KC_Z,      KC_X,    KC_C,    KC_V,   KC_B,    KC_NO,
+        KC_WH_D,        KC_WH_U,   KC_LEFT, KC_RGHT, KC_BSPC,
                                                      KC_BTN2,   KC_BTN1,
                                                                 KC_LCTL,
                                               KC_LSFT, KC_LGUI, KC_LALT,
         // right hand
              KC_NO,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_ESC,
-             KC_BTN1,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_MINS,
-                          KC_M,    KC_N,    KC_E,    KC_I,    KC_O,       KC_ENT,
-             MO(SYMB),    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,    KC_BSLS,
-                                   KC_SPC,  KC_UP,   KC_DOWN, KC_EQL,     KC_NO,
-             MO(MDIA), KC_BTN2,
+             MO(MDIA),    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_NO,
+                          KC_M,    KC_N,    KC_E,    KC_I,    KC_O,       MO(SYMB),
+             KC_NO,       KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_ENT,     KC_NO,
+                                   KC_SPC,  KC_UP,   KC_DOWN, KC_NO,      KC_NO,
+             KC_BTN1, KC_BTN2,
              KC_RCTL,
              KC_RALT, KC_RGUI, KC_RSFT
     ),
+
+/*
+ *
+  !@#$%^&*()
+  `~-_;:<>?/\|'"
+
+    >>=     bind
+    >>      then
+    *>      then
+    ->      to                a -> b: a to b
+    <-      bind              (as it desugars to >>=)
+    <$>     (f)map
+    <$      map-replace by    0 <$ f: "f map-replace by 0"
+    <*>     ap(ply)           (as it is the same as Control.Monad.ap)
+    $                         (none, just as " " [whitespace])
+    .       pipe to           a . b: "b pipe-to a"
+    !!      index
+    !       index / strict    a ! b: "a index b", foo !x: foo strict x
+    <|>     or / alternative  expr <|> term: "expr or term"
+    ++      concat / plus / append
+    []      empty list
+    :       cons
+    ::      of type / as      f x :: Int: f x of type Int
+    \       lambda
+    @       as                go ll@(l:ls): go ll as l cons ls
+    ~       lazy              go ~(a,b): go lazy pair a, b
+ */
+
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      | INS  |           | WH_D |      |   [  |  ]   |      |      |   F12  |
+ * |        |   +  |   /  |   \  |   :  |   !  | INS  |           | WH_D |   @  |   "  |   [  |   ]  |      |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   (  |   )  |   &  |   *  |        |
+ * |        |   =  |   <  |   >  |   -  |   ^  |------|           |------|   #  |   '  |   (  |   )  |   ?  |        |
  * |--------+------+------+------+------+------|      |           | WH_U |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |   {  |   }  |      |      |        |
+ * |        |   *  |   |  |   &  |   $  |   %  |      |           |      |   ~  |   `  |   {  |   }  |   _  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      | Home | End  |      |                                       |      | PgUp | PgDn |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -76,18 +106,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,
-       KC_NO,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+       KC_NO,   KC_PLUS, KC_SLSH, KC_BSLS, KC_COLN, KC_EXLM, KC_INS,
+       KC_NO,   KC_EQL,  KC_LABK, KC_RABK, KC_MINS, KC_CIRC,
+       KC_NO,   KC_ASTR, KC_PIPE, KC_AMPR, KC_DLR,  KC_PERC, KC_NO,
        KC_NO,   KC_NO,   KC_HOME, KC_END,  KC_NO,
                                         KC_NO,   KC_NO,
                                                  KC_TRNS,
                                KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
        KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_WH_D, KC_NO,   KC_LBRC, KC_RBRC, KC_NO,   KC_NO,   KC_F12,
-                KC_CIRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_ASTR, KC_NO,
-       KC_WH_U, KC_NO,   KC_LCBR, KC_RCBR, KC_NO,   KC_NO,   KC_NO,
+       KC_WH_D, KC_AT,   KC_DQUO, KC_LBRC, KC_RBRC, KC_NO,   KC_F12,
+                KC_HASH, KC_QUOT, KC_LPRN, KC_RPRN, KC_QUES, KC_NO,
+       KC_WH_U, KC_TILD, KC_GRV,  KC_LCBR, KC_RCBR, KC_UNDS, KC_NO,
                          KC_NO,   KC_PGUP, KC_PGDN, KC_NO,   KC_NO,
        KC_NO,   KC_NO,
        KC_TRNS,
