d898e4ea5b8e69de07c579af90263adf125f5e22
Adam Szlachta <adam.szlachta@gmail.com>
2017-10-23 18:59:15 +0200

    Improve script

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 
 cd ../..
 where=$1
+skip=$2
 if [ -z "$where" ]; then
     where="colmxDefault"
 fi
 output_dir=~/Dropbox/share/ergo/Teensy/$where
 hex_path=../../ergodox_ez_dktn_osx.hex
 mkdir -p $output_dir
-make dktn_osx && cp $hex_path $output_dir # && teensy_loader_cli --mcu=atmega32u4 -w $hex_path
+if [ "$skip" == "skip" ]; then
+    make dktn_osx && cp $hex_path $output_dir
+else
+    make dktn_osx && cp $hex_path $output_dir && teensy_loader_cli --mcu=atmega32u4 -w $hex_path
+fi
 cd keymaps/dktn_osx
