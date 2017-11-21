5a6f88d0b30b0b7c18e6747fd450a3987c9a9e69
Adam Szlachta <adam.szlachta@gmail.com>
2017-11-21 20:32:36 +0100

    Update to new qmk firmware

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -x
 
-cd ../..
+qmkpath=../../../..
+buildpath=.build/ergodox_ez_dktn.hex
+cd $qmkpath
 where=$1
 skip=$2
 if [ -z "$where" ]; then
     where="colmxDefault"
 fi
 output_dir=~/Dropbox/share/ergo/Teensy/$where
-hex_path=../../ergodox_ez_dktn_osx.hex
 mkdir -p $output_dir
 if [ "$skip" == "skip" ]; then
-    make dktn_osx && cp $hex_path $output_dir
+    make ergodox_ez:dktn && cp $buildpath $output_dir
 else
-    make dktn_osx && cp $hex_path $output_dir && teensy_loader_cli --mcu=atmega32u4 -w $hex_path
+    make ergodox_ez:dktn:teensy && cp $buildpath $output_dir
 fi
-cd keymaps/dktn_osx
+cd keyboards/ergodox_ez/keymaps/dktn
--- keymap.c
+++ keymap.c
@@ -1,4 +1,4 @@
-#include "ergodox.h"
+#include QMK_KEYBOARD_H
 #include "debug.h"
 #include "action_layer.h"
 #include "timer.h"
@@ -45,15 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 
+// tried: LT(SYMB, KC_TAB) instead of MO
+
 #ifndef LINUX_MODE
 // OLS vs. MO
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,      KC_2,    KC_3,    KC_4,   KC_5,    KC_ENT,
-        KC_TAB,         KC_Q,      KC_W,    KC_D,    KC_P,   KC_G,    KC_DELT,
-        MO(SYMB),       KC_A,      KC_R,    KC_S,    KC_T,   KC_F,
-        KC_GRV,         KC_Z,      KC_X,    KC_C,    KC_V,   KC_B,    MO(MDIA),
-        KC_UP,          KC_DOWN,   KC_LEFT, KC_RGHT, KC_BSPC,
+        KC_ESC,           KC_1,      KC_2,    KC_3,    KC_4,   KC_5,    KC_ENT,
+        KC_TAB,           KC_Q,      KC_W,    KC_D,    KC_P,   KC_G,    KC_DELT,
+        MO(SYMB),         KC_A,      KC_R,    KC_S,    KC_T,   KC_F,
+        // LT(SYMB, KC_TAB), KC_A,      KC_R,    KC_S,    KC_T,   KC_F,
+        KC_GRV,           KC_Z,      KC_X,    KC_C,    KC_V,   KC_B,    MO(MDIA),
+        KC_UP,            KC_DOWN,   KC_LEFT, KC_RGHT, KC_BSPC,
                                                      KC_LALT,   KC_NO,
                                                                 KC_LCTL,
                                               KC_LSFT, KC_LGUI, KC_LALT,
@@ -61,6 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_BTN2,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_ESC,
              KC_BTN1,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_BTN1,
                           KC_M,    KC_N,    KC_E,    KC_I,    KC_O,       MO(SYMB),
+                        //   KC_M,    KC_N,    KC_E,    KC_I,    KC_O,       LT(SYMB, KC_ESC),
              MO(MDIA),    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_ENT,     KC_BTN2,
                                    KC_SPC,  KC_UP,   KC_DOWN, KC_WH_U,    KC_WH_D,
              KC_NO,   KC_RALT,
@@ -124,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   !  |   &  |   |  |   \  |   ~  | INS  |           | WH_D |      |   "  |   [  |   ]  |   !  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  ESC   |   /  |   =  |   -  |   :  |   +  |------|           |------|   _  |   '  |   (  |   )  |   ?  |   Tab  |
+ * |  TAB   |   /  |   =  |   -  |   :  |   +  |------|           |------|   _  |   '  |   (  |   )  |   ?  |   ESC  |
  * |--------+------+------+------+------+------|      |           | WH_U |------+------+------+------+------+--------|
  * |        |   <  |   *  |   $  |   >  |   ^  |      |           |      |      |   `  |   {  |   }  |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -143,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        KC_NO,   KC_QUES, KC_AT,   KC_HASH, KC_PERC, KC_NO,   KC_NO,
        KC_NO,   KC_EXLM, KC_AMPR, KC_PIPE, KC_BSLS, KC_TILD, KC_INS,
-       KC_ESC,  KC_SLSH, KC_EQL,  KC_MINS, KC_COLN, KC_PLUS,
+       KC_TAB,  KC_SLSH, KC_EQL,  KC_MINS, KC_COLN, KC_PLUS,
        KC_NO,   KC_LABK, KC_ASTR, KC_DLR,  KC_RABK, KC_CIRC, KC_NO,
        KC_NO,   KC_NO,   KC_HOME, KC_END,  KC_DELT,
                                         KC_TRNS, KC_TRNS,
@@ -152,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
        KC_WH_D, KC_NO,   KC_DQUO, KC_LBRC, KC_RBRC, KC_EXLM, KC_NO,
-                KC_UNDS, KC_QUOT, KC_LPRN, KC_RPRN, KC_QUES, KC_TAB,
+                KC_UNDS, KC_QUOT, KC_LPRN, KC_RPRN, KC_QUES, KC_ESC,
        KC_WH_U, KC_NO,   KC_GRV,  KC_LCBR, KC_RCBR, KC_TRNS, KC_NO,
                          KC_TRNS, KC_PGUP, KC_PGDN, KC_NO,   KC_NO,
        KC_TRNS, KC_TRNS,
@@ -234,8 +238,8 @@ void matrix_init_user(void) {
 #define BLINK_BASE1_ON   1U
 #define BLINK_BASE1_OFF 10U
 
-#define BLINK_BASE2_ON  120U
-#define BLINK_BASE2_OFF 220U
+#define BLINK_BASE2_ON   6U
+#define BLINK_BASE2_OFF 50U
 
 void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
